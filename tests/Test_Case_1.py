8ae6e0dc889d60139c9405c0b07053119b1ec7f2
bayan kassem <bayanka@post.bgu.ac.il>
2025-03-31 19:11:22 +0300

    added new function for modularity and improved both tests

--- tests/Test_Case_1.py
+++ tests/Test_Case_1.py
@@ -14,99 +14,134 @@ def page(browser):
     yield page
     page.close()
 
-# def test_case_1_airbnb_search_and_analyze_results(page):
-#
-#     page.goto("https://www.airbnb.com")
-#
-#
-#
-#     page.wait_for_selector("#bigsearch-query-location-input", timeout=50000)
-#
-#
-#
-#     page.fill("#bigsearch-query-location-input", "tel aviv")
-#
-#     page.wait_for_selector("[data-testid='structured-search-input-field-guests-button']", timeout=50000)
-#     page.click("[data-testid='structured-search-input-field-guests-button']")
-#     page.wait_for_selector("[data-testid='stepper-adults-increase-button']", timeout=50000)
-#     page.click("[data-testid='stepper-adults-increase-button']")
-#     page.click("[data-testid='stepper-adults-increase-button']")
-#
-#     page.click("[data-testid='structured-search-input-search-button']")
-#
-#     page.click("[data-testid='category-bar-filter-button']")
-#     max_price=45
-#     while True:
-#         page.fill("#price_filter_max", str(max_price+1))
-#         page.wait_for_timeout(100)
-#         max_price+=1
-#         page.click("#price_filter_max-label")
-#
-#         show_places_link = page.locator(
-#             'xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
-#
-#         link_text = show_places_link.inner_text()
-#         while link_text=="loading":
-#             link_text = show_places_link.inner_text()
-#
-#         if any(char.isdigit() for char in link_text):
-#             places_count = int(''.join(filter(str.isdigit, link_text)))
-#             if places_count > 0:
-#                 print(f"Found {places_count} places!")
-#                 break
-#
-#
-#     page.click('xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
-#     try:
-#
-#         room_title = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[1]').inner_text()
-#         print(f"Room title: {room_title}")
-#         subtitle = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/span[1]/span[1]').inner_text()
-#         print(f"Subtitle: {subtitle}")
-#         room_description = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[3]').inner_text()
-#         print(f"Room description: {room_description}")
-#
-#         date_range = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[4]/span/span[1]').inner_text()
-#         print(f"Date range: {date_range}")
-#
-#         price_per_night = page.locator('span.a8jt5op.atm_3f_idpfg4.atm_7h_hxbz6r.atm_7i_ysn8ba.atm_e2_t94yts.atm_ks_zryt35.atm_l8_idpfg4.atm_vv_1q9ccgz.atm_vy_t94yts.aze35hn.atm_mk_stnw88.atm_tk_idpfg4.dir.dir-ltr')
-#         if price_per_night.count() == 0:
-#             price_per_night = page.locator('span.a8jt5op.atm_3f_idpfg4.atm_7h_hxbz6r.atm_7i_ysn8ba.atm_e2_t94yts.atm_ks_zryt35.atm_l8_idpfg4.atm_vv_1q9ccgz.atm_vy_t94yts.a1ugchtf.atm_mk_stnw88.atm_tk_idpfg4.dir.dir-ltr')
-#         print(price_per_night.inner_text())
-#
-#         total_price = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[5]/div[2]/div/div/span[3]/div/button/div/div').inner_text()
-#         print(total_price)
-#
-#
-#     except Exception as e:
-#         print(f"Error extracting details: {e}")
+
+def got_to_tel_aviv_with_2_adults(page):
+    page.goto("https://www.airbnb.com")
+
+    page.wait_for_selector("#bigsearch-query-location-input", timeout=50000)
+
+    page.fill("#bigsearch-query-location-input", "tel aviv")
+
+    page.wait_for_selector("[data-testid='structured-search-input-field-guests-button']", timeout=50000)
+    page.click("[data-testid='structured-search-input-field-guests-button']")
+    page.wait_for_selector("[data-testid='stepper-adults-increase-button']", timeout=50000)
+    page.click("[data-testid='stepper-adults-increase-button']")
+    page.click("[data-testid='stepper-adults-increase-button']")
+
+    page.click("[data-testid='structured-search-input-search-button']")
+    page.wait_for_timeout(5000)
+
+
+def test_case_1_airbnb_search_and_analyze_results(page):
+    got_to_tel_aviv_with_2_adults(page)
+
+    page.wait_for_selector("[itemprop='itemListElement']", timeout=50000)
+
+    items = page.query_selector_all("[itemprop='itemListElement']")
+
+
+    max_rating = 0.0
+    title=""
+    subtitle=""
+    room_description=""
+    date_description = ""
+    price=""
+    total_price=""
+
+    for item in items:
+
+        rating_element = item.query_selector(".r4a59j5")
+        if rating_element:
+
+            rating_text = rating_element.inner_text()
+            if "out of" in rating_text:
+                rating_value = float(rating_text.split(" ")[0])
+
+                if rating_value > max_rating:
+                    max_rating = rating_value
+                    title=item.query_selector("[data-testid='listing-card-title']").inner_text()
+                    subtitle = item.query_selector("[data-testid='listing-card-subtitle']").inner_text()
+                    subtitle_spans = item.query_selector_all("[data-testid='listing-card-subtitle'] span span")
+                    if len(subtitle_spans) > 0:
+                        room_description = subtitle_spans[0].inner_text()
+                    if len(subtitle_spans) > 4 :
+                        date_description = subtitle_spans[3].inner_text()
+                    price = item.query_selector("span._hb913q").inner_text()
+                    total_price = item.query_selector("div._tt122m span[aria-hidden='true']").inner_text()
+    print()
+    print("details about the apartment with the highest rating:")
+    print(f"title: {title}")
+    print(f"subtitle: {subtitle}")
+    print(f"room description: {room_description}")
+    print(f"date description: {date_description}")
+    print(f"price per night: {price}")
+    print(f"total price: {total_price}")
+
+
+    page.click("[data-testid='category-bar-filter-button']")
+    max_price=45
+    while True:
+        page.fill("#price_filter_max", str(max_price+1))
+        page.wait_for_timeout(100)
+        max_price+=1
+        page.click("#price_filter_max-label")
+
+        show_places_link = page.locator(
+            'xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
+
+        link_text = show_places_link.inner_text()
+        while link_text=="loading":
+            link_text = show_places_link.inner_text()
+
+        if any(char.isdigit() for char in link_text):
+            places_count = int(''.join(filter(str.isdigit, link_text)))
+            if places_count > 0:
+                break
+
+
+    page.click('xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
+
+
+    title = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[1]').inner_text()
+
+    subtitle = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/span[1]/span[1]').inner_text()
+
+
+    room_description = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[3]').inner_text()
+
+
+    date_description = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[4]/span/span[1]').inner_text()
+
+
+    price_per_night = page.locator('span.a8jt5op.atm_3f_idpfg4.atm_7h_hxbz6r.atm_7i_ysn8ba.atm_e2_t94yts.atm_ks_zryt35.atm_l8_idpfg4.atm_vv_1q9ccgz.atm_vy_t94yts.aze35hn.atm_mk_stnw88.atm_tk_idpfg4.dir.dir-ltr')
+    if price_per_night.count() == 0:
+        price_per_night = page.locator('span.a8jt5op.atm_3f_idpfg4.atm_7h_hxbz6r.atm_7i_ysn8ba.atm_e2_t94yts.atm_ks_zryt35.atm_l8_idpfg4.atm_vv_1q9ccgz.atm_vy_t94yts.a1ugchtf.atm_mk_stnw88.atm_tk_idpfg4.dir.dir-ltr')
+
+    price=price_per_night.inner_text()
+    total_price = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[5]/div[2]/div/div/span[3]/div/button/div/div').inner_text()
+
+
+    print()
+    print("details about the apartment with the lowest price:")
+    print(f"title: {title}")
+    print(f"subtitle: {subtitle}")
+    print(f"room description: {room_description}")
+    print(f"date description: {date_description}")
+    print(f"price per night: {price}")
+    print(f"total price: {total_price}")
+
 
 def test_case_2_airbnb_search_and_reserve(page):
-        page.goto("https://www.airbnb.com")
-
-        page.wait_for_selector("#bigsearch-query-location-input", timeout=50000)
-
-        page.fill("#bigsearch-query-location-input", "Tel Aviv")
-
-        page.wait_for_selector("[data-testid='structured-search-input-field-guests-button']", timeout=50000)
-        page.click("[data-testid='structured-search-input-field-guests-button']")
-        page.wait_for_selector("[data-testid='stepper-adults-increase-button']", timeout=50000)
-        page.click("[data-testid='stepper-adults-increase-button']")
-        page.click("[data-testid='stepper-adults-increase-button']")
-
-        page.click("[data-testid='structured-search-input-search-button']")
-        page.wait_for_timeout(5000)
+        got_to_tel_aviv_with_2_adults(page)
         page.wait_for_selector("[itemprop='itemListElement']", timeout=50000)
 
         items = page.query_selector_all("[itemprop='itemListElement']")
-        print()
+
 
         max_rating = 0.0
-        max_rating_url = ""
         max_rating_element = None
 
         for item in items:
-            item_url = item.query_selector("a").get_attribute('href') if item.query_selector("a") else "No URL"
 
             rating_element = item.query_selector(".r4a59j5")
             if rating_element:
@@ -117,12 +152,10 @@ def test_case_2_airbnb_search_and_reserve(page):
 
                     if rating_value > max_rating:
                         max_rating = rating_value
-                        max_rating_url = item_url
                         max_rating_element = item
 
 
         max_rating_element.click()
-        print(f"Clicked on the item with the highest rating: {max_rating_url}, Rating: {max_rating}")
         new_tab = page.context.wait_for_event("page")
         new_tab.wait_for_timeout(5000)
         popup_button = new_tab.query_selector(f"xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[1]/button")
@@ -130,28 +163,23 @@ def test_case_2_airbnb_search_and_reserve(page):
             popup_button.click()
 
         price_locator = new_tab.locator('span._hb913q').first
-
-
         price_text = price_locator.text_content()
-
-        print("Price per night:", price_text)
-
         date_locator_check_in = new_tab.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/button/div[1]/div[2]')
-        print("Check-in Date:", date_locator_check_in.text_content())
-
         date_locator_check_out = new_tab.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/button/div[2]/div[2]')
-        print("Check-out Date:", date_locator_check_out.text_content())
         guests_locator = new_tab.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div[2]/label/div[2]/div')
-        print("guest count:", guests_locator.text_content())
-        nights_price = new_tab.locator(
-            'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[1]/div[1]/span[2]')
-        print("price for all nights:", nights_price.text_content())
-        airbnb_service = new_tab.locator(
-            'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[1]/div[2]/span[2]')
-        print("airbnb service:", airbnb_service.text_content())
-        total_price = new_tab.locator(
-            'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span')
-        print("total price:", total_price.text_content())
+        nights_price = new_tab.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[1]/div[1]/span[2]')
+        airbnb_service = new_tab.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[1]/div[2]/span[2]')
+        total_price = new_tab.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span')
+
+        print()
+        print("Reservation Details for the apartment with the highest rating:")
+        print(f"price per night: {price_text}")
+        print(f"Check-in Date: {date_locator_check_in.text_content()}")
+        print(f"Check-out Date: {date_locator_check_out.text_content()}")
+        print(f"guest count: {guests_locator.text_content()}")
+        print(f"price for all nights: {nights_price.text_content()}")
+        print(f"airbnb service: {airbnb_service.text_content()}")
+        print(f"total price: {total_price.text_content()}")
 
         new_tab.click('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[3]/div/button/span[1]/span')
         new_tab.fill('input[name="phoneInputphone-login"]', "123456789")
