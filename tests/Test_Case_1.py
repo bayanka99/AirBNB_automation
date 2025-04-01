0e50f6d0f7307d80fff9b3815f3d38bfbbef0abc
bayan kassem <bayanka@post.bgu.ac.il>
2025-04-01 08:47:21 +0300

    improved the code, and added some comments

--- tests/Test_Case_1.py
+++ tests/Test_Case_1.py
@@ -32,21 +32,14 @@ def got_to_tel_aviv_with_2_adults(page):
     page.wait_for_timeout(5000)
 
 
-def test_case_1_airbnb_search_and_analyze_results(page):
-    got_to_tel_aviv_with_2_adults(page)
-
-    page.wait_for_selector("[itemprop='itemListElement']", timeout=50000)
-
-    items = page.query_selector_all("[itemprop='itemListElement']")
-
-
+def print_details_highest_rating(items):
     max_rating = 0.0
-    title=""
-    subtitle=""
-    room_description=""
+    title = ""
+    subtitle = ""
+    room_description = ""
     date_description = ""
-    price=""
-    total_price=""
+    price = ""
+    total_price = ""
 
     for item in items:
 
@@ -54,17 +47,18 @@ def test_case_1_airbnb_search_and_analyze_results(page):
         if rating_element:
 
             rating_text = rating_element.inner_text()
+            #sometimes it might be "new" which is not a valid rating, there is a rating only if it contains "out of"
             if "out of" in rating_text:
                 rating_value = float(rating_text.split(" ")[0])
 
                 if rating_value > max_rating:
                     max_rating = rating_value
-                    title=item.query_selector("[data-testid='listing-card-title']").inner_text()
+                    title = item.query_selector("[data-testid='listing-card-title']").inner_text()
                     subtitle = item.query_selector("[data-testid='listing-card-subtitle']").inner_text()
                     subtitle_spans = item.query_selector_all("[data-testid='listing-card-subtitle'] span span")
                     if len(subtitle_spans) > 0:
                         room_description = subtitle_spans[0].inner_text()
-                    if len(subtitle_spans) > 4 :
+                    if len(subtitle_spans) > 4:
                         date_description = subtitle_spans[3].inner_text()
                     price = item.query_selector("span._hb913q").inner_text()
                     total_price = item.query_selector("div._tt122m span[aria-hidden='true']").inner_text()
@@ -78,19 +72,20 @@ def test_case_1_airbnb_search_and_analyze_results(page):
     print(f"total price: {total_price}")
 
 
+def filter_lowest_price(page):
     page.click("[data-testid='category-bar-filter-button']")
-    max_price=45
+    max_price = 45
     while True:
-        page.fill("#price_filter_max", str(max_price+1))
+        page.fill("#price_filter_max", str(max_price + 1))
         page.wait_for_timeout(100)
-        max_price+=1
+        max_price += 1
         page.click("#price_filter_max-label")
 
         show_places_link = page.locator(
             'xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
 
         link_text = show_places_link.inner_text()
-        while link_text=="loading":
+        while link_text == "loading":
             link_text = show_places_link.inner_text()
 
         if any(char.isdigit() for char in link_text):
@@ -99,27 +94,21 @@ def test_case_1_airbnb_search_and_analyze_results(page):
                 break
 
 
-    page.click('xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
-
-
+def print_details_lowest_price(page):
     title = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[1]').inner_text()
 
     subtitle = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/span[1]/span[1]').inner_text()
 
-
     room_description = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[3]').inner_text()
 
-
     date_description = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[4]/span/span[1]').inner_text()
-
 
     price_per_night = page.locator('span.a8jt5op.atm_3f_idpfg4.atm_7h_hxbz6r.atm_7i_ysn8ba.atm_e2_t94yts.atm_ks_zryt35.atm_l8_idpfg4.atm_vv_1q9ccgz.atm_vy_t94yts.aze35hn.atm_mk_stnw88.atm_tk_idpfg4.dir.dir-ltr')
     if price_per_night.count() == 0:
         price_per_night = page.locator('span.a8jt5op.atm_3f_idpfg4.atm_7h_hxbz6r.atm_7i_ysn8ba.atm_e2_t94yts.atm_ks_zryt35.atm_l8_idpfg4.atm_vv_1q9ccgz.atm_vy_t94yts.a1ugchtf.atm_mk_stnw88.atm_tk_idpfg4.dir.dir-ltr')
 
-    price=price_per_night.inner_text()
+    price = price_per_night.inner_text()
     total_price = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div[1]/main/div[2]/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div[5]/div[2]/div/div/span[3]/div/button/div/div').inner_text()
-
 
     print()
     print("details about the apartment with the lowest price:")
@@ -129,5 +118,26 @@ def test_case_1_airbnb_search_and_analyze_results(page):
     print(f"date description: {date_description}")
     print(f"price per night: {price}")
     print(f"total price: {total_price}")
+
+
+def test_case_1_airbnb_search_and_analyze_results(page):
+    got_to_tel_aviv_with_2_adults(page)
+
+    page.wait_for_selector("[itemprop='itemListElement']", timeout=50000)
+
+    # this is the list of items as a result of the search query
+    items = page.query_selector_all("[itemprop='itemListElement']")
+
+    print_details_highest_rating(items)
+
+    filter_lowest_price(page)
+
+
+    page.click('xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[2]/footer/div/a')
+
+    print_details_lowest_price(page)
+
+
+
 
 
--- tests/Test_Case_2.py
+++ tests/Test_Case_2.py
@@ -32,49 +32,38 @@ def got_to_tel_aviv_with_2_adults_and_1_child(page):
     page.click("[data-testid='structured-search-input-search-button']")
     page.wait_for_timeout(5000)
 
-def test_case_2_airbnb_search_and_reserve(page):
-    got_to_tel_aviv_with_2_adults_and_1_child(page)
-    page.wait_for_selector("[itemprop='itemListElement']", timeout=50000)
-
-    items = page.query_selector_all("[itemprop='itemListElement']")
-
+def get_max_rating_element(items):
     max_rating = 0.0
     max_rating_element = None
-
     for item in items:
-
         rating_element = item.query_selector(".r4a59j5")
         if rating_element:
-
             rating_text = rating_element.inner_text()
+            #sometimes it might be "new" which is not a valid rating, there is a rating only if it contains "out of"
             if "out of" in rating_text:
                 rating_value = float(rating_text.split(" ")[0])
-
                 if rating_value > max_rating:
                     max_rating = rating_value
                     max_rating_element = item
+    return max_rating_element
 
-    max_rating_element.click()
-    new_tab = page.context.wait_for_event("page")
-    new_tab.wait_for_timeout(5000)
-    popup_button = new_tab.query_selector(f"xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[1]/button")
-    if popup_button:
-        popup_button.click()
 
-    price_locator = new_tab.locator('span._hb913q').first
+def print_reservation_box_details(page):
+    price_locator = page.locator('span._hb913q').first
     price_text = price_locator.text_content()
-    date_locator_check_in = new_tab.locator(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/button/div[1]/div[2]')
-    date_locator_check_out = new_tab.locator(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/button/div[2]/div[2]')
-    guests_locator = new_tab.locator(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div[2]/label/div[2]/div')
-    nights_price = new_tab.locator(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[1]/div[1]/span[2]')
-    airbnb_service = new_tab.locator(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[1]/div[2]/span[2]')
-    total_price = new_tab.locator(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span')
+    date_locator_check_in = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/button/div[1]/div[2]')
+    date_locator_check_out = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/button/div[2]/div[2]')
+    guests_locator = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div[2]/label/div[2]/div')
+    price_blocks_locator = page.locator('div._14omvfj')
+
+    titles_and_prices = list()
+    # loop through each component in total price and extract the component's price and title
+    for block in price_blocks_locator.all():
+        title = block.locator('div._10d7v0r button div.l1x1206l').text_content()
+        amount = block.locator('span._1k4xcdh').text_content()
+        titles_and_prices.append([title,amount])
+
+    total_price = page.locator('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span')
 
     print()
     print("Reservation Details for the apartment with the highest rating:")
@@ -82,12 +71,29 @@ def test_case_2_airbnb_search_and_reserve(page):
     print(f"Check-in Date: {date_locator_check_in.text_content()}")
     print(f"Check-out Date: {date_locator_check_out.text_content()}")
     print(f"guest count: {guests_locator.text_content()}")
-    print(f"price for all nights: {nights_price.text_content()}")
-    print(f"airbnb service: {airbnb_service.text_content()}")
+    for title_and_price in titles_and_prices:
+        print(f"{title_and_price[0]}: {title_and_price[1]}")
     print(f"total price: {total_price.text_content()}")
 
-    new_tab.click(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[3]/div/button/span[1]/span')
+
+def test_case_2_airbnb_search_and_reserve(page):
+    got_to_tel_aviv_with_2_adults_and_1_child(page)
+    page.wait_for_selector("[itemprop='itemListElement']", timeout=50000)
+
+    #this is the list of items as a result of the search query
+    items = page.query_selector_all("[itemprop='itemListElement']")
+    max_rating_element=get_max_rating_element(items)
+
+    max_rating_element.click()
+    new_tab = page.context.wait_for_event("page")
+    new_tab.wait_for_timeout(5000)
+    #sometimes a pop-up shows that informs that some information has been translated, which must be closed to continue our test
+    popup_button = new_tab.query_selector(f"xpath=/html/body/div[9]/div/div/section/div/div/div[2]/div/div[1]/button")
+    if popup_button:
+        popup_button.click()
+
+    print_reservation_box_details(new_tab)
+
+    new_tab.click('xpath=/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[3]/div/button/span[1]/span')
     new_tab.fill('input[name="phoneInputphone-login"]', "123456789")
-    new_tab.click(
-        'xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div/main/div/div/div/div[1]/div[2]/div/div[1]/div/div[7]/div/div/div/div[2]/div/div/div/form/div/div[4]/button/span[1]/span')
+    new_tab.click('xpath=/html/body/div[5]/div/div/div[1]/div/div[3]/div/main/div/div/div/div[1]/div[2]/div/div[1]/div/div[7]/div/div/div/div[2]/div/div/div/form/div/div[4]/button/span[1]/span')
